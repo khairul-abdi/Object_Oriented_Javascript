0f1f3255c247cac6f8482c23cfd4f7ad97e6187b
Linuxer <linuxer@abdi.local>
2020-02-10 04:40:14 +0700

    ADD classes Constructor and Metod Chaining

---
+++ The Net Ninja/4. Method Chaining/script.js
@@ -0,0 +1,28 @@
+class User {
+  constructor(email, name) {
+    this.email = email
+    this.name = name
+    this.score = 0
+  }
+
+  logIn() {
+    console.log(`${this.email}, just logged in`);
+    return this
+  }
+
+  logOut() {
+    console.log(`${this.email}, just logged out`);
+    return this
+  }
+
+  updateScore() {
+    this.score++
+    console.log(`${this.email} score is ${this.score}`);
+    return this
+  }
+}
+
+const userOne = new User('kabdi384@gmail.com', 'khairul')
+const userTwo = new User('abdi@gmail.com', 'abdi')
+
+userOne.logIn().updateScore().updateScore().logOut()
